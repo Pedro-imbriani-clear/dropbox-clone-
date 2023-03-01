513fbce4c37307f35cfd1d810b6a16b075b2f596
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-03-01 12:00:52 -0300

    configuring the views

--- app/public/src/DropBoxController.js
+++ app/public/src/DropBoxController.js
@@ -3,6 +3,9 @@ class DropBoxController{
         this.btnSendFileEl = document.querySelector("#btn-send-file");
         this.inputFileEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector("#react-snackbar-root");
+        this.progressBarEl = this.snackModalEl.querySelector(".mc-progress-bar-fg");
+        this.namefileEl = document.querySelector('.filename');
+        this.timeleftEl = document.querySelector('.timeleft');
         this.initEvents();
 
     }
@@ -13,8 +16,13 @@ class DropBoxController{
         });
         this.inputFileEl.addEventListener('change', (event)=>{
             this.uploadTask(event.target.files);
-            this.snackModalEl.style.display = 'block';
+            this.modalShow();
+            this.inputFileEl.value='';
+
         });
+    }
+    modalShow(show = true){
+        this.snackModalEl.style.display = (show)?'block':'name';
     }
     uploadTask(files){
         let promises =[];
@@ -23,6 +31,7 @@ class DropBoxController{
                 let ajax = new XMLHttpRequest();
                 ajax.open('POST',"/upload")
                 ajax.onload = event=>{
+                    this.modalShow(false);
                     try {
                         resolve(JSON.parse(ajax.responseText))
                     }catch(e){
@@ -31,13 +40,51 @@ class DropBoxController{
                     }
                 }
                 ajax.onerror = event=>{
-                    reject(event)
+                    this.modalShow(false);
+                    
+                    reject(event);
+                };
+                ajax.upload.onprogress = event =>{
+                    this.uploadProgress(event, file);
+                   
                 }
-                let formData = new FormData()
-                formData.append('input-file',file)
-                ajax.send(formData)
+                let formData = new FormData();
+                formData.append('input-file',file);
+                this.startUploadTime = Date.now();
+                ajax.send(formData);
             }))
         });
         return Promise.all(promises)
     }
+    uploadProgress(event,file){
+        let timespent = Date.now() - this.startUploadTime;
+        let loaded = event.loaded;
+        let total = event.total;
+        let porcent = parseInt ((loaded/total)*100);
+        let timeleft = ((100- porcent)* timespent)/ porcent;
+
+        this.progressBarEl .style.width = `${porcent}%`;
+        this.namefileEl.innerHTML = file.name;
+        this.timeleftEl.innerHTML = this.formatTimeToHuman(timeleft);
+        
+
+    }
+    formatTimeToHuman(duration){
+        let seconds = parseInt((duration/1000)% 60);
+        let minutes =   parseInt((duration/(1000*60))% 60);
+        let hours =   parseInt((duration/(1000*60*60))% 24);
+
+        if (hours>0){
+            return`${hours}horas, ${minutes}minutos e ${seconds}segundos`;
+        }
+        if (minutes>0){
+            return` ${minutes}minutos e ${seconds}segundos`;
+        }
+        if (seconds>0){
+            return` ${segunds0}segundos`;
+    
+        }
+        return "";
+    }
+
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
-
+var formiidable = require("formidable");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post("/upload",(req,res)=>{
+  let form = new formiidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions:true
+  });
+  form.parse(req,(err,fields, files) =>{
+    res.json({
+      files
+    });
 
+  });
+  res.json(req.body);
+});
 module.exports = router;
